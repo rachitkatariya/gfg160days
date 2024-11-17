7aca455c381459c3f7d1a0baf0a1c2bd5e96a347
rachitkatariyaa <rachitkumar2953@gmail.com>
2024-11-17 11:17:42 +0530

    first commit

---
+++ Day 1/secondLargestElement.cpp
@@ -0,0 +1,21 @@
+// User function template for C++
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+
+    int getSecondLargest(vector<int> &arr) {
+    int largest = INT_MIN, secondLargest = INT_MIN;
+
+    for (int num : arr) {
+        if (num > largest) {
+            secondLargest = largest;
+            largest = num;
+        } else if (num > secondLargest && num != largest) {
+            secondLargest = num;
+        }
+    }
+
+    return (secondLargest == INT_MIN) ? -1 : secondLargest;
+}
+};
