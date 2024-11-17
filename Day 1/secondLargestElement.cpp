7660e4b30e6b5b4ded5ec37dbf12a45501caa960
rachitkatariyaa <rachitkumar2953@gmail.com>
2024-11-17 11:29:04 +0530

    first commit

--- Day 1/secondLargestElement.cpp
+++ Day 1/secondLargestElement.cpp
@@ -1,21 +1,38 @@
-// User function template for C++
+#include <iostream>
+#include <vector> 
+#include <climits> 
+using namespace std;
+
 class Solution {
-  public:
-    // Function returns the second
-    // largest elements
-
+public:
     int getSecondLargest(vector<int> &arr) {
-    int largest = INT_MIN, secondLargest = INT_MIN;
-
-    for (int num : arr) {
-        if (num > largest) {
-            secondLargest = largest;
-            largest = num;
-        } else if (num > secondLargest && num != largest) {
-            secondLargest = num;
+        int n = arr.size();
+        if (n < 2) {
+            return -1; 
         }
-    }
 
-    return (secondLargest == INT_MIN) ? -1 : secondLargest;
-}
+        int largest = INT_MIN;
+        int secondLargest = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        return (secondLargest == INT_MIN) ? -1 : secondLargest;
+    }
 };
+
+int main() {
+    Solution s;
+    vector<int> arr = {10, 20, 4, 45, 99}; 
+
+    int result = s.getSecondLargest(arr);
+    cout << "Second Largest: " << result << endl; 
+
+    return 0;
+}
